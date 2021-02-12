ca406d645bf0e64124ccbbe8f62f768924d6c34c
r-4bb1t <r4bb1t.github@gmail.com>
2021-02-13 05:28:00 +0900

    fix: close svg 수정

--- svgs/close-icon-svg.tsx
+++ svgs/close-icon-svg.tsx
@@ -6,11 +6,11 @@ export default function CloseIconSVG(props: any) {
       xmlns="http://www.w3.org/2000/svg"
       width={26}
       height={25.997}
-      viewBox="0 0 26 25.997"
+      viewBox="0 0 25 25"
       {...props}
     >
-      <line x1="0" y1="0" x2="25" y2="25" stroke="#7131b7" strokeLinecap="round" strokeWidth="3" />
-      <line x1="25" y1="0" x2="0" y2="25" stroke="#7131b7" strokeLinecap="round" strokeWidth="3" />
+      <line x1="3" y1="3" x2="22" y2="22" stroke="#7131b7" strokeLinecap="round" strokeWidth="2" />
+      <line x1="22" y1="3" x2="3" y2="22" stroke="#7131b7" strokeLinecap="round" strokeWidth="2" />
     </svg>
   );
 }
