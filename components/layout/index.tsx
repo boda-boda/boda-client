f1a107f6025c9869d75eb058493165fb35f52e53
r-4bb1t <r4bb1t.github@gmail.com>
2021-02-14 01:25:21 +0900

    add: 카테고리 컴포넌트 추가

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,6 +1,7 @@
 import React, { ReactChild } from 'react';
 import { BannerStyleType } from '../../common/types';
 import Banner from '../banner';
+import Category from '../category';
 import Footer from '../footer';
 import Header from '../header';
 import * as S from './styles';
@@ -16,6 +17,7 @@ export default function Layout({ children }: LayoutProps) {
         <Header />
         <S.ContentWrapper>
           <Banner bannerStyle={BannerStyleType.AD} />
+          <Category list={['홈', '인생', '라이프라이프']} />
           <S.ContentContainer>{children}</S.ContentContainer>
         </S.ContentWrapper>
         <Footer />
