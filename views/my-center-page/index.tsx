e73c00e2981a1ec7f6a7624665f4459ec02f7bc0
r-4bb1t <r4bb1t.github@gmail.com>
2021-02-16 04:13:20 +0900

    add: 나의 센터 정보 추가

---
+++ views/my-center-page/index.tsx
@@ -0,0 +1,86 @@
+import * as S from './styles';
+import Layout from '../../components/layout';
+import Banner from '../../components/banner';
+import { BannerStyleType } from '../../common/types';
+import Category from '../../components/category';
+import SlideLeftButtonSVG from '../../svgs/slide-left-button-svg';
+import SlideRightButtonSVG from '../../svgs/slide-right-button-svg';
+import { useState } from 'react';
+
+export default function MyCenterPage() {
+  const centerImage = [
+    'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2020-07/kitten-510651.jpg',
+    'https://icatcare.org/app/uploads/2018/06/Layer-1704-1200x630.jpg',
+    'https://www.humanesociety.org/sites/default/files/styles/1240x698/public/2018/08/kitten-440379.jpg',
+  ];
+  const [imageIndex, setImageIndex] = useState(0);
+  return (
+    <Layout>
+      <>
+        <Banner
+          bannerStyle={BannerStyleType.SECTION}
+          sectionIndex={1}
+          title="나의 센터 정보"
+          subtitle="우리 센터 정보를 등록할 수 있습니다."
+        />
+        <Category list={['홈', '나의 센터 정보']} />
+        <S.InnerContent>
+          <S.MyCenterPage>
+            <S.Section>
+              <S.SectionTitle>센터 정보</S.SectionTitle>
+              <S.Table>
+                <tbody>
+                  <tr>
+                    <td rowSpan={4} className="profile">
+                      <S.ProfileImageContainer>
+                        <S.ProfileImage src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/close-up-of-cat-wearing-sunglasses-while-sitting-royalty-free-image-1571755145.jpg" />
+                      </S.ProfileImageContainer>
+                    </td>
+                    <th>이름</th>
+                    <td>보다 재가센터</td>
+                    <th>위치</th>
+                    <td>서대문구 통일로 123</td>
+                  </tr>
+                  <tr>
+                    <th>전화</th>
+                    <td>02-000-0000</td>
+                    <th>홈페이지</th>
+                    <td>www.boda.kr</td>
+                  </tr>
+                  <tr>
+                    <th>팩스</th>
+                    <td>02-000-0000</td>
+                    <th>이메일</th>
+                    <td>bodacenter@boda.kr</td>
+                  </tr>
+                  <tr>
+                    <th>센터 소개</th>
+                    <td colSpan={3}>센터 소개글이 표시됩니다.</td>
+                  </tr>
+                </tbody>
+              </S.Table>
+            </S.Section>
+            <S.Section>
+              <S.SectionTitle>센터 이미지</S.SectionTitle>
+              <S.CenterImageContainer>
+                <S.CenterImage src={centerImage[imageIndex]} />
+                <S.ButtonContainer>
+                  <S.ButtonDiv
+                    onClick={() =>
+                      setImageIndex((imageIndex + centerImage.length - 1) % centerImage.length)
+                    }
+                  >
+                    <SlideLeftButtonSVG />
+                  </S.ButtonDiv>
+                  <S.ButtonDiv onClick={() => setImageIndex((imageIndex + 1) % centerImage.length)}>
+                    <SlideRightButtonSVG />
+                  </S.ButtonDiv>
+                </S.ButtonContainer>
+              </S.CenterImageContainer>
+            </S.Section>
+          </S.MyCenterPage>
+        </S.InnerContent>
+      </>
+    </Layout>
+  );
+}
---
+++ views/my-center-page/styles.tsx
@@ -0,0 +1,134 @@
+import styled from 'styled-components';
+import {
+  CONTENT_WIDTH,
+  FLEX_COLUMN_CENTER_START,
+  FLEX_COLUMN_START_START,
+  FLEX_ROW_CENTER_CENTER,
+  FLEX_ROW_CENTER_END,
+  THEME,
+} from '../../constant';
+
+export const MyCenterPage = styled.div`
+  width: 100%;
+`;
+
+export const Table = styled.table`
+  width: 100%;
+  margin-top: 24px;
+  border-top: 2px solid ${THEME.MAIN};
+  border-collapse: collapse;
+  th,
+  td {
+    border-bottom: 1px solid ${THEME.GRAY_LINE};
+    padding: 21px 12px;
+    color: ${THEME.GRAY_FONT};
+    font-size: 14px;
+    text-align: left;
+    vertical-align: text-top;
+  }
+  th {
+    width: 84px;
+    background: ${THEME.HEADER_BACKGROUND};
+    color: ${THEME.PLACEHOLDER_ACTIVE_LOCATION_END};
+    font-weight: 500;
+  }
+  .profile {
+    width: 168px;
+    vertical-align: middle;
+    padding: 0;
+  }
+  .personality {
+    padding: 0 12px;
+  }
+  .career {
+    width: 50%;
+    text-align: center;
+    border-right: 1px solid ${THEME.GRAY_LINE};
+  }
+  th.career {
+    padding: 13px 0;
+  }
+  .right {
+    border-right: none;
+  }
+`;
+
+export const ProfileImageContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  ${FLEX_ROW_CENTER_CENTER};
+`;
+
+interface ProfileImageProps {
+  src: string;
+}
+
+export const ProfileImage = styled.div<ProfileImageProps>`
+  width: 118px;
+  height: 118px;
+  border-radius: 10px;
+  background: ${THEME.BACKGROUND};
+  background-image: url(${(props) => props.src});
+  background-size: cover;
+  background-position: center;
+`;
+
+export const Section = styled.div`
+  position: relative;
+  width: 100%;
+  ${FLEX_COLUMN_START_START};
+  padding-top: 40px;
+  padding-bottom: 20px;
+`;
+
+export const SectionTitle = styled.div`
+  font-size: 22px;
+  font-weight: 500;
+  color: ${THEME.PLACEHOLDER_ACTIVE_LOCATION_END};
+`;
+
+export const InnerContent = styled.div`
+  width: ${CONTENT_WIDTH}px;
+  height: 100%;
+  color: ${THEME.GRAY_LINE};
+  ${FLEX_COLUMN_CENTER_START};
+`;
+
+interface ImageProps {
+  src: string;
+}
+
+export const CenterImageContainer = styled.div`
+  margin-top: 24px;
+  margin-bottom: 40px;
+  position: relative;
+  width: 100%;
+  height: 640px;
+`;
+
+export const CenterImage = styled.div<ImageProps>`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background-image: url(${(props) => props.src});
+  background-repeat: no-repeat;
+  background-position: center;
+  transition: 0.2s ease;
+`;
+export const ButtonContainer = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  ${FLEX_ROW_CENTER_CENTER};
+  justify-content: space-between;
+  width: 100%;
+  height: 100%;
+`;
+
+export const ButtonDiv = styled.div`
+  width: 50px;
+  height: 50px;
+  opacity: 0.25;
+  background-color: ${THEME.PLACEHOLDER_ACTIVE_LOCATION_END};
+  cursor: pointer;
+`;
