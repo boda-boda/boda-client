9ca6697bb8b7b128d039d7939a6d86e5fd1383c6
강관훈 <khkang@promedius.ai>
2021-03-13 12:08:13 +0900

    fix: remove word

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function MyApp({ Component, pageProps }: AppProps) {
         isLoggedIn: false,
       }}
     >
-      <Component {...pageProps} />;
+      <Component {...pageProps} />
     </CareCenterProvider>
   );
 }
