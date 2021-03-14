c3db67be353926325009613ae520f84948eda7db
강관훈 <khkang@promedius.ai>
2021-03-14 12:41:04 +0900

    fix: 리프레시토큰 만료시 발생하는 오류 해소

--- common/hooks/auth.tsx
+++ common/hooks/auth.tsx
@@ -9,33 +9,39 @@ export const useSoftRefresh = () => {
 
   useEffect(() => {
     (async () => {
+      if (!careCenter) return;
       if (!careCenter.isValidating) return;
-      if (careCenter && careCenter.expiresIn > new Date()) return; // 현재 토큰이 활성화되어 있으면 요청을 할 이유가 없음
+      if (careCenter.expiresIn > new Date()) return; // 현재 토큰이 활성화되어 있으면 요청을 할 이유가 없음
 
       try {
-        const respose = await axios.post('/api/auth/refresh');
-        axios.defaults.headers['Authorization'] = `Bearer ${respose.data.accessToken}`;
+        const response = await axios.post('/api/auth/refresh');
+        const statusCode = response.data.statusCode;
+
+        // accessToken을 다시 요청할 때 실패시 slient하게 하기 위해 오류 던지지 않고 콘솔로 대체
+        if (statusCode >= 400) {
+          switch (statusCode) {
+            case 400:
+              break;
+            case 401:
+              alert('자동 로그인 세션이 만료되어 로그아웃되었습니다.');
+          }
+          throw new Error();
+        }
+
+        axios.defaults.headers['Authorization'] = `Bearer ${response.data.accessToken}`;
         careCenterDispatch({
           type: 'LOGIN',
           payload: {
-            accessToken: respose.data.accessToken,
-            expiresIn: addSeconds(Date.now(), parseInt(respose.data.expiresIn)),
-            careCenter: respose.data.careCenter,
+            accessToken: response.data.accessToken,
+            expiresIn: addSeconds(Date.now(), parseInt(response.data.expiresIn)),
+            careCenter: response.data.careCenter,
           },
         });
-      } catch ({ response }) {
-        switch (response.status) {
-          case 401:
-          case 400:
-            break;
-          default:
-            console.log('TOKEN REFRESH');
-            console.log('예측할 수 없는 오류입니다. 관리자에게 문의 부탁드립니다.');
-        }
+      } catch (e) {
         careCenterDispatch({
           type: 'LOGOUT',
         });
       }
     })();
-  });
+  }, []);
 };
