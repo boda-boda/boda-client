4e59c8c3b0259fe0a65d576d198fa75ea822136f
r-4bb1t <r4bb1t.github@gmail.com>
2021-03-18 23:38:50 +0900

    add: add og:description

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -21,6 +21,10 @@ export default function MyApp({ Component, pageProps }: AppProps) {
           content="간편하게 원하는 요양보호사를 탐색해보세요! 재가센터의 요양보호사 관리 및 매칭서비스, 돌봄입니다."
         />
         <meta
+          property="og:description"
+          content="간편하게 원하는 요양보호사를 탐색해보세요! 재가센터의 요양보호사 관리 및 매칭서비스, 돌봄입니다."
+        />
+        <meta
           property="og:image"
           content="https://user-images.githubusercontent.com/52532871/111497282-51a2f900-8784-11eb-93ff-1b699a13b582.png"
         />
