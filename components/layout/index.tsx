4d3ebdd7689c13fc0eaa1a5d4c67fe3c4bdd298f
r-4bb1t <r4bb1t.github@gmail.com>
2021-03-21 23:58:29 +0900

    feat: top으로 scroll하는 버튼 추가

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useSoftRefresh } from '../../common/hooks/auth';
+import ArrowUpSVG from '../../svgs/arrow-up-svg';
 import Footer from '../footer';
 import Header from '../header';
 import * as S from './styles';
@@ -11,10 +12,17 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   useSoftRefresh();
 
+  const handleTop = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+  };
+
   return (
     <S.Layout>
       <Header />
       <S.ContentWrapper>{children}</S.ContentWrapper>
+      <S.TopButton onClick={handleTop}>
+        <ArrowUpSVG />
+      </S.TopButton>
       <Footer />
     </S.Layout>
   );
--- components/layout/styles.tsx
+++ components/layout/styles.tsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { CONTENT_WIDTH } from '../../constant';
+import { CONTENT_WIDTH, THEME } from '../../constant';
 
 export const Layout = styled.div`
   width: 100%;
@@ -20,4 +20,19 @@ export const ContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+export const TopButton = styled.button`
+  position: fixed;
+  right: 70px;
+  bottom: 50px;
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  outline: none;
+  border: none;
+  background: ${THEME.MAIN};
+  box-shadow: 0 3px 6px 0 rgba(0, 0, 0, 0.16);
+  padding-top: 5px;
+  cursor: pointer;
 `;
