aed4fff15922a8fe66c9a30c8d281b1682f5d5ad
khkang <khkang@promedius.ai>
2021-03-30 02:17:23 +0900

    feat: 요양보호사 추가 및 수정시 validation 구현

---
+++ common/lib/validate.ts
@@ -0,0 +1,51 @@
+import CreateCareGiverRequest from '../../components/care-giver-edit/model/create-care-giver-request';
+
+export const isStringOnlyDigit = (str: string) => {
+  return str.match(/^[0-9]*$/);
+};
+
+export const validatePhoneNumber = (number: string) => {
+  if (!number) return;
+
+  if (number.length < 9 || number.length > 11)
+    return '전화번호는 9~11자의 숫자만 입력하실 수 있습니다.';
+
+  if (!isStringOnlyDigit(number)) return '전화번호는 숫자(0~9)만 입력하실 수 있습니다.';
+};
+
+export const validateBirthday = (birthDay: string) => {
+  if (!birthDay) return;
+
+  if (birthDay.length !== 8) return '생년월일은 숫자(0~9) 8자리를 입력해주세요.';
+
+  if (!isStringOnlyDigit(birthDay)) return '생년월일은 숫자(0~9)만 입력하실 수 있습니다.';
+};
+
+export const validateEmail = (email: string) => {
+  if (!email) return;
+
+  // https://www.edureka.co/blog/javascript-email-validation/
+  if (!email.match(/^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/))
+    return '올바른 이메일 양식을 입력해주세요.';
+};
+
+export const validateCareWorker = (createCareGiverRequest: CreateCareGiverRequest) => {
+  const { name, birthDay, phoneNumber } = createCareGiverRequest;
+
+  if (!name) {
+    alert('이름을 입력해 주세요.');
+    return false;
+  }
+
+  if (validatePhoneNumber(phoneNumber)) {
+    alert(validatePhoneNumber(phoneNumber));
+    return false;
+  }
+
+  if (validateBirthday(birthDay)) {
+    alert(validateBirthday(birthDay));
+    return false;
+  }
+
+  return true;
+};
