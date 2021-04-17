3071fae501cc5d8e5a438cb986aaad5c5570e1e4
khkang <khkang@promedius.ai>
2021-04-17 17:49:03 +0900

    feat: 사설 페이지 분리

--- views/error/index.tsx
+++ views/error/index.tsx
@@ -1,18 +1,33 @@
 import Layout from '../../components/layout';
-import Banner from '../../components/banner';
-import { BannerStyleType } from '../../common/types';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useEffect } from 'react';
+import { useCareCenter } from '../../context/care-center';
+import { useSoftRefresh } from '../../common/hooks/auth';
+import { PRIVATE_ROUTES } from '../../constant';
 
-export default function Main() {
+export default function ErrorPage() {
   const router = useRouter();
+  const careCenter = useCareCenter();
+
+  useSoftRefresh();
 
   useEffect(() => {
+    if (careCenter.isValidating) return;
+
+    if (PRIVATE_ROUTES.includes(router.pathname)) {
+      router.push('/landing');
+      return;
+    }
+
     if (router.asPath) {
       router.push(router.asPath);
     }
-  }, [router]);
+  }, [careCenter, router]);
+
+  if (careCenter.isValidating) {
+    return <div></div>;
+  }
 
   return (
     <>
