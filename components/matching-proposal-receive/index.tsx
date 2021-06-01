52d89559d6ee7038b756d67d22ec4faba3cac214
bjg <birdy_birdy@naver.com>
2021-06-02 01:25:23 +0900

    fix: 매칭 제안서 확인 페이지 더 잘 보이게

--- components/matching-proposal-receive/index.tsx
+++ components/matching-proposal-receive/index.tsx
@@ -97,52 +97,48 @@ export default function MatchingProposalReceive({ isFilled }: MatchingProposalPr
               </S.SectionTitleContainer>
               <S.InfoTable>
                 <tbody>
+                  <th className="th" rowSpan={4}>
+                    사진
+                  </th>
+                  <td className="td" rowSpan={4}>
+                    <S.ProfileImageContainer>
+                      <S.ProfileImage src={recipient.profile} />
+                    </S.ProfileImageContainer>
+                  </td>
+
+                  <th className="top th">이름</th>
+                  <td className="top td">{recipient.name}</td>
+
                   <tr>
-                    <td rowSpan={9} className="recipientProfile">
-                      <S.ProfileImageContainer>
-                        <S.ProfileImage src={recipient.profile} />
-                      </S.ProfileImageContainer>
-                    </td>
-                    <th>이름</th>
-                    <td className="left">{recipient.name}</td>
-                    <th>성별</th>
-                    <td className="right">{recipient.isFemale ? '여자' : '남자'}</td>
-                  </tr>
-                  <tr>
-                    <th>나이</th>
-                    <td className="right">{recipient.age}세</td>
-                    <th>등급</th>
-                    <td className="select left">{recipient.grade}등급</td>
-                  </tr>
-                  <tr>
-                    <th>돌봄 시간</th>
-                    <td colSpan={1} className="wide">
-                      {recipient.schedule}
-                    </td>
-                    <th>종교</th>
-                    <td colSpan={1} className="">
-                      {recipient.religion}
+                    <th className="th">기본 정보</th>
+                    <td className="td">
+                      {recipient.isFemale ? '여자' : '남자'} / {recipient.age}세
                     </td>
                   </tr>
                   <tr>
-                    <th>거주 형태</th>
-                    <td colSpan={1} className="wide">
-                      {recipient.familyType}
-                    </td>
-                    <th>휴대전화</th>
-                    <td colSpan={1} className="wide">
-                      {recipient.phoneNumber}
-                    </td>
+                    <th className="th">등급</th>
+                    <td className="td">{recipient.grade}등급</td>
                   </tr>
                   <tr>
-                    <th rowSpan={1}>주소</th>
+                    <th className="th">종교</th>
+                    <td className="td">{recipient.religion}</td>
+                  </tr>
+                  <tr>
+                    <th className="th">돌봄 시간</th>
+                    <td className="td">{recipient.schedule}</td>
+                    <th className="th">거주 형태</th>
+                    <td className="td">{recipient.familyType}</td>
+                  </tr>
+
+                  <tr>
+                    <th className="th">주소</th>
                     <td colSpan={3}>
                       {recipient.zipCode && `(${recipient.zipCode})`} {recipient.address}{' '}
                       {recipient.detailAddress}
                     </td>
                   </tr>
                   <tr>
-                    <th>요구 사항</th>
+                    <th className="th">요구 사항</th>
                     <td colSpan={3} className="overtd">
                       <S.TdFlexBox>
                         {recipient.recipientMetas
@@ -163,7 +159,7 @@ export default function MatchingProposalReceive({ isFilled }: MatchingProposalPr
                     </td>
                   </tr>
                   <tr>
-                    <th>세부 사항</th>
+                    <th className="th">세부 사항</th>
                     <td colSpan={3} className="select wide">
                       {recipient.description}
                     </td>
@@ -175,18 +171,18 @@ export default function MatchingProposalReceive({ isFilled }: MatchingProposalPr
               <S.SectionTitleContainer>
                 <S.SectionTitle>근무 조건</S.SectionTitle>
               </S.SectionTitleContainer>
-              <S.InfoTable>
+              <S.InfoTableWorkingCondition>
                 <tbody>
                   <tr>
-                    <th>시급</th>
-                    <td>{matchingProposal.hourlyWage}원</td>
+                    <th className="th">시급</th>
+                    <td className="td">{matchingProposal.hourlyWage}원</td>
                   </tr>
                   <tr>
                     <th>비고</th>
                     <td>{matchingProposal.description}</td>
                   </tr>
                 </tbody>
-              </S.InfoTable>
+              </S.InfoTableWorkingCondition>
             </S.Section>
             <S.CompleteSection>
               <S.EditButton onClick={handleClickAcceptButton}>수락 하기</S.EditButton>
